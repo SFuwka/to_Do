1b3cef110efeae67cb68e87b4f5905cd545e2bb8
SFuwka <Alexjavac@yandex.ru>
2020-08-05 15:49:43 +0300

    align nav items

--- public/nav-event-handler.js
+++ public/nav-event-handler.js
@@ -1,8 +1,16 @@
 const navButton = document.getElementById('navButton');
 const collapsedContent = document.getElementById('collapsedContent')
+const navItems = document.getElementsByClassName('nav-item')
 navButton.addEventListener('click', function () {
     toggleDisplay(collapsedContent);
+    elemsToRight();
 })
+
+function elemsToRight() {
+    for(item of navItems){
+        item.style.textAlign = 'right'
+    }
+}
 
 function toggleDisplay(elem) {
     if (elem.style.display === 'block') {
